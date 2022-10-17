3972c03f000c30d98d90b05fc7e8bf93209697cf
unknown <kim.yen@dsegom.com>
2022-10-17 17:25:21 +0800

    feat: ReduxThunk fetch API with axios

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import App from './App'
 
 import { store } from './app/store'
 import { Provider } from 'react-redux'
+import { fetchUsers } from './features/users/usersSlice'
+
+store.dispatch(fetchUsers())
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 )
